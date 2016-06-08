e7c0093d46b1299751c24bd563ea7591296d812a
tianminglei <shmilyfox190@163.com>
2016-06-08 17:34:46 +0800

    上传测试

---
+++ Application/Home/Controller/BookController.class.php
@@ -0,0 +1,73 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class BookController extends Controller {
+	public function index()
+	{
+		dwr.engine._remoteHandleCallback('2','0',{flag:"0",RESULT:"0",'_msg':null,'PAGE_SIZE':"100",IP:"136.142.26.240",'ZW_INFO_CODE':"BD3120002",METHOD:"insertVoteXx",msg:null,'PAGE_INDEX':"1",WN:"oAPXLjgHzqnczhDOpDDT39Msb1oEY"});
+
+
+		callCount=1
+		page=/WechatWeb/wx/vote/votePeopleMx.html?zw_info_code=LF3160039&wn=oAPXLjgHzqnczhDOpDDT42Msb1oEY
+		httpSessionId=10gHXWwGvVNjS9glSHPj2JSmYQZ27kdsQlv0QFQWZH1WyfDFvhx1!1957597696
+		scriptSessionId=AA0CBE9FB90164F9E0E55CF74FCC9338515
+		c0-scriptName=WXService
+		c0-methodName=excute
+		c0-id=0
+		c0-param0=string:WXS
+		c0-param1=string:insertVoteXx
+		c0-e1=string:LF3160039
+		c0-e2=string:oAPXLjgHzqnczhDOpDDT42Msb1oEY
+		c0-e3=string:insertVoteXx
+		c0-param2=Object_Object:{zw_info_code:reference:c0-e1, wn:reference:c0-e2, method:reference:c0-e3}
+		batchId=2
+
+		callCount=1
+		page=/WechatWeb/wx/vote/votePeopleMx.html?zw_info_code=LF3160039&wn=oAPXLjgHzqnczhDOpDDT42Msb1oEY
+		httpSessionId=10gHXWwGvVNjS9glSHPj2JSmYQZ27kdsQlv0QFQWZH1WyfDFvhx1!1957597696
+		scriptSessionId=AA0CBE9FB90164F9E0E55CF74FCC9338515
+		c0-scriptName=WXService
+		c0-methodName=excute
+		c0-id=0
+		c0-param0=string:WXS
+		c0-param1=string:getVotePeopleMx
+		c0-e1=string:LF3160039
+		c0-e2=string:oAPXLjgHzqnczhDOpDDT42Msb1oEY
+		c0-e3=string:getVotePeopleMx
+		c0-param2=Object_Object:{zw_info_code:reference:c0-e1, wn:reference:c0-e2, method:reference:c0-e3}
+		batchId=3
+
+		callCount=1
+		page=/WechatWeb/wx/vote/votePeopleMx.html?zw_info_code=LF3160039&wn=oAPXLjgHzqnczhDOpDDT42Msb1oEY
+		httpSessionId=10gHXWwGvVNjS9glSHPj2JSmYQZ27kdsQlv0QFQWZH1WyfDFvhx1!1957597696
+		scriptSessionId=AA0CBE9FB90164F9E0E55CF74FCC9338515
+		c0-scriptName=WXService
+		c0-methodName=excute
+		c0-id=0
+		c0-param0=string:WXS
+		c0-param1=string:insertVoteXx
+		c0-e1=string:LF3160039
+		c0-e2=string:oAPXLjgHzqnczhDOpDDT42Msb1oEY
+		c0-e3=string:insertVoteXx
+		c0-param2=Object_Object:{zw_info_code:reference:c0-e1, wn:reference:c0-e2, method:reference:c0-e3}
+		batchId=5
+
+		已经投票 不成功
+		callCount=1
+		page=/WechatWeb/wx/vote/votePeopleMx.html?zw_info_code=LF3160039&wn=oAPXLjgHzqnczhDOpDDT42Msb1oEY
+		httpSessionId=10gHXWwGvVNjS9glSHPj2JSmYQZ27kdsQlv0QFQWZH1WyfDFvhx1!1957597696
+		scriptSessionId=AA0CBE9FB90164F9E0E55CF74FCC9338718
+		c0-scriptName=WXService
+		c0-methodName=excute
+		c0-id=0
+		c0-param0=string:WXS
+		c0-param1=string:insertVoteXx
+		c0-e1=string:LF3160039
+		c0-e2=string:oAPXLjgHzqnczhDOpDDT42Msb1oEY
+		c0-e3=string:insertVoteXx
+		c0-param2=Object_Object:{zw_info_code:reference:c0-e1, wn:reference:c0-e2, method:reference:c0-e3}
+		batchId=2
+
+	}
+   
+}
